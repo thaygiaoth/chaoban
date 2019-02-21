d270af2946a12121fd22762f36b94944e3bf92e1
Nguyễn Trung Hiếu <thaygiaoth@gmail.com>
2019-02-21 04:56:58 +0000

    Ghi chú trong src/main.c là tiếng Việt

--- src/main.c
+++ src/main.c
@@ -1,35 +1,35 @@
-#include <ncurses.h>			/* ncurses.h tu dong them stdio.h */  
-#include <string.h>			/* De dung cac ham getstr(), strlen(), getch() */
+#include <ncurses.h>	/* ncurses.h tự động thêm stdio.h */
+#include <string.h>	/* Để dùng các hàm getstr(), strlen(), getch() */
 
 void main(void)
 {
-	char nhap[]="Nhap ten ban: ";	/* Bien luu thong bao nhap ten */
- 	char hoten[40];			/* Bien luu ten nhap vao */
- 	int row, col;			/* Bien luu so dong va so cot */
+	char nhap[]="Nhap ten ban: ";	/* Biến lưu thông báo nhập tên */
+ 	char hoten[40];			/* Biến lưu tên nhập vào */
+ 	int row, col;			/* Biến lưu số dòng và số cột của màn hình terminal */
 
- 	initscr();			/* Bat dau che do curses */
- 	getmaxyx(stdscr, row, col);	/* Lay so dong va so cot cua man hinh terminal */
+ 	initscr();			/* Bắt đầu chế độ curses */
+ 	getmaxyx(stdscr, row, col);	/* Lấy số dòng và số cột của màn hình terminal */
 
 
-	/* In ra so dong va so cot cua terminal
- 	Gia su so dong la xx, so cot la xxx hoac nguoc lai
- 	chuoi 'Che do man hinh: xx dong yyy cot' co do dai 32 */
+	/* In ra số dòng và số cột của màn hình terminal
+	Giả sử số dòng là xx, số cột là yyy hoặc ngược lại
+	chuỗi 'Che do man hinh: xx dong yyy cot' có độ dài 32 */
 	mvprintw((row/2)-2, (col-32)/2, "Che do man hinh: %d dong %d cot", row, col);
 
 	mvprintw(row/2, (col-32)/2, "%s", nhap);
 
  	getstr(hoten);
 
- 	/* Xoa man hinh */
+ 	/* Xóa màn hình */
  	erase();
 
- 	/* In giua man hinh dong chu Chao ban ten_ban
- 	chuoi 'Chao ban ' co do dai la 9 */
+ 	/* In giữa màn hình dòng chữ Chao ban tên_bạn
+ 	chuỗi 'Chao ban ' có độ dài là 9 */
  	if (row % 2 == 0)
  		mvprintw((row/2), (col-(strlen(hoten)+9))/2, "Chao ban %s", hoten);
  	else
 		mvprintw((row/2)+1, (col-(strlen(hoten)+9))/2, "Chao ban %s", hoten);	
 
- 	getch();	/* Nhan 1 phim de ket thuc chuong trinh */
- 	endwin();	/* Ket thuc che do curses*/
+ 	getch();	/* Nhấn 1 phím để kết thúc chương trình */
+ 	endwin();	/* Kết thúc chế độ curses*/
 }
